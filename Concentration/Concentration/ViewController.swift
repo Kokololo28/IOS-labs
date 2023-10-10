5b91d72c85e988fe12aa712ae9a8a1d7cf204464
Kokololo28 <102355878+Kokololo28@users.noreply.github.com>
2023-10-10 09:26:05 +0300

    Add files via upload

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Nikita Hrabovskyi on 19.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var image: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
