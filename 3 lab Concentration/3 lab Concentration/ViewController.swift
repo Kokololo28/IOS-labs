d1a1491b8955e782460eda9a2b53263088cc04b5
Kokololo28 <102355878+Kokololo28@users.noreply.github.com>
2023-10-31 09:29:14 +0200

    Add files via upload

---
+++ 3 lab Concentration/3 lab Concentration/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  3 lab Concentration
+//
+//  Created by Nikita Hrabovskyi on 10.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var Views: [CardView]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
+
+
